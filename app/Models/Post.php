c6562e7ad0fc0d218aa0f907dd4c9f268a401c4e
roman <romanbalitskij9@gmail.com>
2019-02-03 16:29:13 +0200

    admin LTE

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    //
+    use Sluggable;
+
+    protected $fillable = ['title','content'];
+
+    public function category(){
+      return  $this->hasOne(Category::class);
+    }
+
+    public function author(){
+      return $this->hasOne(User::class);
+    }
+
+    public function tags(){
+      return $this->belongsToMany(
+          Tag::class,
+          'post_tags',
+          'post_id',
+          'tag_id'
+      );
+    }
+
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -27,4 +27,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
 }
